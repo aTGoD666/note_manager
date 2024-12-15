133e06de51d41891f126c4ea4585e4cce4041ac9
aTGoD666 <devidsatan619@gmail.com>
2024-12-15 19:35:18 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+username = input("Имя: ") # имя пользователя
+title = input("Заголовок темы: ")  # заголовок
+title2 = input("Заголовок урока: ")  # заголовок
+title3 = input("Заголовок задания: ")  # заголовок
+title_all = [title,title2,title3]  # заголовок
+content = input("Описание: ") # описание заметки
+status = input("Статус: ") # статус заметки
+created_date = datetime.strptime(input("Дата: "), "%d-%m-%y") # дата создание заметки (формат дд-мм-гггг)
+issue_date = datetime.strptime(input("Дата: "), "%d-%m-%y") # дата истечения заметки (формат дд-мм-гггг)
+print(title_all)
