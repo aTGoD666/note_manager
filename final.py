cb7ae77e2e1d89f771a5d0ed45b1d4a7361032b2
aTGoD666 <devidsatan619@gmail.com>
2024-12-20 23:12:46 +0300

    Update final.py
    all_info - теперь словарь с ключами

--- final.py
+++ final.py
@@ -8,14 +8,11 @@ content = input("Описание: ") # описание заметки
 status = input("Статус: ") # статус заметки
 created_date = datetime.strptime(input("Дата: "), "%d-%m-%y") # дата создание заметки (формат дд-мм-гггг)
 issue_date = datetime.strptime(input("Дата: "), "%d-%m-%y") # дата истечения заметки (формат дд-мм-гггг)
-all_info = [username,
-            title,
-            title2,
-            title3,
-            content,
-            status,
-            created_date,
-            issue_date,
-            title_all]
+all_info = {"имя":username,
+            "Описание":content,
+            "Статус":status,
+            "создание заметки":created_date,
+            "истечения заметки":issue_date,
+            "заголовок":title_all}
 
 print(all_info)
